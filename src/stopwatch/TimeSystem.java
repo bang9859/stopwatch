e3916ca553759798431cbb5416421f9c2d3dc3bd
bang9859 <kokozz9859@gmail.com>
2024-11-13 17:56:20 +0900

    Fix run method

--- src/stopwatch/TimeSystem.java
+++ src/stopwatch/TimeSystem.java
@@ -6,7 +6,19 @@ import java.util.Calendar;
 public class TimeSystem implements Runnable {
 	private boolean isRun = true;
 	private int sec = 1;
+	boolean isHold = false;
 	private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+	InputSystem input = InputSystem.getInstance();
+
+	private TimeSystem() {
+
+	}
+
+	private static TimeSystem instance = new TimeSystem();
+
+	public static TimeSystem getInstance() {
+		return instance;
+	}
 
 	public int getSec() {
 		return this.sec;
@@ -19,13 +31,28 @@ public class TimeSystem implements Runnable {
 	@Override
 	public void run() {
 		while (isRun) {
+
+			if (input.getTimeHold())
+				isHold = true;
+
+			while (isHold) {
+				if (input.getTimeRerun())
+					break;
+				if (input.getTimeStop()) {
+					isRun = false;
+					break;
+				}
+			}
 			Calendar cal = Calendar.getInstance();
 			String msg = sdf.format(cal.getTime());
 			System.out.printf(msg + "[%d sec]\n", sec++);
 
 			try {
 				Thread.sleep(1000);
-			} catch (InterruptedException e) {
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			if (input.getTimeStop()) {
 				isRun = false;
 			}
 		}
