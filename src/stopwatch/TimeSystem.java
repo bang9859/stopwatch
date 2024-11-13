838b909b9fb49f96f5fa4c47d29d994b05ddc820
bang9859 <kokozz9859@gmail.com>
2024-11-13 13:50:02 +0900

    Feat constructor and getter,run method

--- src/stopwatch/TimeSystem.java
+++ src/stopwatch/TimeSystem.java
@@ -1,5 +1,33 @@
 package stopwatch;
 
-public class TimeSystem {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class TimeSystem implements Runnable {
+	private boolean isRun = true;
+	private int sec = 1;
+	private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.S");
 	
+	public int getSec() {
+		return this.sec;
+	}
+	
+	public void TimerSystem(int sec) {
+		this.sec = sec;
+	}
+	
+	@Override
+	public void run() {
+		while (isRun) {
+			Calendar cal = Calendar.getInstance();
+			String msg = sdf.format(cal.getTime());
+			System.out.printf(msg +"[%d sec]",sec++);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				isRun = false;
+			}
+		}
+	}
+
 }
