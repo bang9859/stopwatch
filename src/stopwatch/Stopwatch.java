6b9d40ae5b8e8156c871f1b3e443939cd389db92
bang9859 <kokozz9859@gmail.com>
2024-11-13 15:55:40 +0900

    Feat play method

--- src/stopwatch/Stopwatch.java
+++ src/stopwatch/Stopwatch.java
@@ -1,9 +1,9 @@
 package stopwatch;
 
-import java.util.Scanner;
-
 public class Stopwatch {
-	private Scanner sc = new Scanner(System.in);
+
+	TimeSystem time = new TimeSystem();
+	InputSystem input = new InputSystem();
 	private Stopwatch() {
 
 	}
@@ -15,6 +15,36 @@ public class Stopwatch {
 	}
 
 	public void run() {
+		showMenu();
+		runStopwatch();
+
+		showResult();
+	}
+
+
+	private void showMenu() {
+		System.err.println("[q] STOP");
+		System.err.println("[h] HOLD");
+		System.err.println("[a] RERUN");
+	}
+
+	private void runStopwatch() {
+		Thread timeThread = new Thread(time);
+		Thread inputThread = new Thread(input);
+		timeThread.start();
+		inputThread.start();
+		if(input.getTimeHold()) {
+			timeThread.interrupt();
+		}else if(input.getTimeHold()&&input.getTimeRerun()) {
+			timeThread.start();
+		}else if(input.getTimeStop()) {
+			timeThread.interrupt();
+			System.out.printf(">>> %d분 %d초 소요됨",time.getSec()/60,time.getSec()%60);
+		}
 		
+	}
+
+	private void showResult() {
+
 	}
 }
--- src/stopwatch/TimeSystem.java
+++ src/stopwatch/TimeSystem.java
@@ -6,28 +6,30 @@ import java.util.Calendar;
 public class TimeSystem implements Runnable {
 	private boolean isRun = true;
 	private int sec = 1;
-	private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.S");
-	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
 	public int getSec() {
 		return this.sec;
 	}
-	
+
 	public void TimerSystem(int sec) {
 		this.sec = sec;
 	}
-	
+
 	@Override
 	public void run() {
 		while (isRun) {
 			Calendar cal = Calendar.getInstance();
 			String msg = sdf.format(cal.getTime());
-			System.out.printf(msg +"[%d sec]",sec++);
+			System.out.printf(msg + "[%d sec]\n", sec++);
+
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				isRun = false;
 			}
 		}
+
 	}
 
 }
